f4ff9aedf2b88b5798c9bcd6acc22ae4734cd87f
Pattaradanai Thanomsittikul <datas.pookan@gmail.com>
2021-07-22 16:58:09 +0700

    Add files via upload

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+/* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-resume/blob/master/LICENSE)*/
+
+window.addEventListener("DOMContentLoaded", (event) => {
+  const sideNav = document.body.querySelector("#sideNav");
+  if (sideNav) {
+    new bootstrap.ScrollSpy(document.body, {
+      target: "#sideNav",
+      offset: 74,
+    });
+  }
+
+  const navbarToggler = document.body.querySelector(".navbar-toggler");
+  const responsiveNavItems = [].slice.call(
+    document.querySelectorAll("#navbarResponsive .nav-link")
+  );
+  responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItem.addEventListener("click", () => {
+      if (window.getComputedStyle(navbarToggler).display !== "none") {
+        navbarToggler.click();
+      }
+    });
+  });
+});
